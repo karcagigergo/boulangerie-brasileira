1aa5f81fb208b502792b4d07bbc6ab2f1a14c5e1
Gergo Karcagi <karcagigergo@gmail.com>
2021-05-08 15:47:56 -0300

    fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,11 +15,13 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @product.user = current_user
     @product.save
     redirect_to product_path(@product)
   end
 
   def edit
+    @product.user = current_user
   end
 
   def update
