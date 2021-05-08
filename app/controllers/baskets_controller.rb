0b828c11cc81c34ee2da8608def23cf1b9e30602
Eduardo Scovino <eduardo.scovino@gmail.com>
2021-05-08 11:37:17 -0300

    baskets controller

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,13 +1,28 @@
 class BasketsController < ApplicationController
+  before_action :set_baskets, only: [:show, :update, :destroy]
 
   def show
-
   end
 
-  def uptdate
+  def update
+    @basket.update(basket_params)
 
+    redirect_to basket_path(@basket)
   end
 
   def destroy
+    @basket.destroy
+
+    redirect_to products_path
+  end
+
+  private
+
+  def set_baskets
+    @basket = Basket.find(params[:id])
+  end
+
+  def basket_params
+    params.require(:basket).permit(:status, :user_id)
   end
 end
