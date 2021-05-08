2b9d9bcf7b42f5b9ab0ebb94d189051a0f3e5b6c
Gergo Karcagi <karcagigergo@gmail.com>
2021-05-08 13:28:33 -0300

    finished validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,16 +1,5 @@
 class Order < ApplicationRecord
-  # before_validation :create_basket, if: :basket_exists?
   belongs_to :basket
   belongs_to :product
-
-  # private
-
-  # def create_basket
-  #   binding.pry
-  #   Basket.create(user_id: current_user.id, status: true)
-  # end
-
-  # def basket_exists?
-  #   !self.basket
-  # end
+  validates :product_id, uniqueness: { scope: :basket_id, message: "is already in the basket" }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :orders
+  validates :name, uniqueness: true, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
 end
