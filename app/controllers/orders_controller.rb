d9fe3fa8035efa4b746900ed99ab0aeca0050548
Eduardo Scovino <eduardo.scovino@gmail.com>
2021-05-11 21:28:16 -0300

    debugging the navbar

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,24 +8,24 @@ class OrdersController < ApplicationController
     #if user has basket, get the basket id
 
     if current_user.basket
-      @basket = current_user.basket
+      basket = current_user.basket
     else
-      @basket = Basket.create(user: current_user)
+      basket = Basket.create(user: current_user)
     end
 
-    @order = Order.new
-    @order.basket = @basket
-    @order.product = Product.find(params[:product_id])
+    order = Order.new
+    order.basket = basket
+    order.product = Product.find(params[:product_id])
 
-    if @order.save
-      redirect_to basket_path(@basket)
+    if order.save
+      redirect_to basket_path(basket)
     else
       render 'products/show'
     end
   end
 
   def destroy
-    @order.destroy
+    order.destroy
 
     redirect_to basket_path(current_user.basket)
   end
@@ -33,7 +33,7 @@ class OrdersController < ApplicationController
  private
 
   def set_orders
-    @order = Order.find(params[:id])
+    order = Order.find(params[:id])
   end
 
 end
