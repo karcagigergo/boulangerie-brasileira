4e0dadd1e82931ddee280844ed4bc1860d78401b
Gergo Karcagi <karcagigergo@gmail.com>
2021-05-14 15:29:42 -0300

    fixed-footer on all pages and implemented sweetalert on products delete button

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -10,3 +10,14 @@ const initSweetalert = (selector, options = {}, callback = () => {}) => {
 };
 
 export { initSweetalert };
+
+// initSweetalert('#sweet-alert', {
+//   title: "Are you sure?",
+//   text: "This action cannot be reversed",
+//   icon: "warning"
+// }, (value) => {
+//   if (value) {
+//     const link = document.querySelector('#delete-link');
+//     link.click();
+//   }
+// });
